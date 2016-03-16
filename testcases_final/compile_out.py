87261834f0ab29a6530225c20f9fb23a6f26283e
David <rebhuhnd@oregonstate.edu>
2016-03-15 21:55:17 -0700

    Added new test cases.

--- testcases_final/compile_out.py
+++ testcases_final/compile_out.py
@@ -1,12 +1,12 @@
 ## Run this script to generate output of cases.
+## Note: this does not remove .out files that have been generated, they just overwrite the ones that exist.
 
 import os
+import subprocess
+import time
 
-for infile in os.listdir("./"):
-	if infile.endswith(".in"):
-		parts = infile.split(".")
-		outfile = parts[0]+".out"
-		print "Generating " + outfile + " from " + infile + "..."
-		os.system("rm *.out")
-		os.system("python "+infile+" > "+outfile)
-
+for infile in os.listdir("./"):	
+    if infile.endswith(".in"):
+        parts = infile.split(".")
+        outfile = parts[0]+".out"				
+        os.system("python " + infile + " > " + outfile)
