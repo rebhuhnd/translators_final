cbf6710c2e510dbbb972542b124e7aba741069d2
David <rebhuhnd@oregonstate.edu>
2016-03-17 18:49:41 -0700

    Updated auto-tester.

--- anubis.bash
+++ anubis.bash
@@ -1,11 +1,39 @@
 ## Named after the Egyptian god of the afterlife who judged souls.
 ## This is a bash version of the auto judge.
+FAILED_TO_COMPILE=()
+PASSED=()
+FAILED=()
 
 for i in testcases_final/*.in; do
-    cat "$i" | python hw6_compiler.py > temp.c 2>/dev/null
-    clang temp.c -o temp
-    diff <( ./temp ) <( python "$i" ) && echo $i PASSED
+	echo Testing $i ...
+    cat "$i" | python hw6_compiler.py > temp.c #2>/dev/null
+
+	if clang temp.c -o temp -w
+	then
+
+		if diff <( ./temp ) <( python "$i" )
+		then
+			echo $i PASSED
+			PASSED+=($i)
+		else
+			echo $i FAILED: Different output.
+			FAILED+=($i)
+		fi
+	else
+		echo $i FAILED: Compiling
+		FAILED_TO_COMPILE+=($i)
+    fi
 done
 
 rm temp
 rm temp.c
+
+echo "SUMMARY:"
+echo "PASSED:"
+printf '%s\n' "${PASSED[@]}"
+
+echo "FAILED TO COMPILE:"
+printf '%s\n' "${FAILED_TO_COMPILE[@]}"
+
+echo "FAILED, BUT COMPILED:"
+printf '%s\n' "${FAILED[@]}"
